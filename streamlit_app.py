25939fbdd7a1ed18a823a3d64b0b6acf36790b3d
Kiran5000 <nunavathkirannayak2000@gmail.com>
2024-03-17 15:05:53 +0530

    update all the files

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,8 @@ from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
 
+GOOGLE_API_KEY = st.secrets["google_api_key"]
+
 load_dotenv()
 os.getenv("GOOGLE_API_KEY")
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
